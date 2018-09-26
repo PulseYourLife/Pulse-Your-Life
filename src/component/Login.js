4bf320a1ac5ec7486c5c39514fb55eb680a10680
David Vaca <spm1dh@gmail.com>
2018-09-26 10:55:14 -0500

    Relation between buttons and views

--- src/component/Login.js
+++ src/component/Login.js
@@ -8,14 +8,27 @@ import InputLabel from '@material-ui/core/InputLabel';
 import LockIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
-import './Login.css'
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import {Register} from './Register';
+import {Statistics} from './Statistics';
+import './Login.css';
 export class Login extends React.Component{
     constructor(props) {
             super(props);
+            localStorage.setItem('isLoggedIn', false);
     }
-
     render(){
+            localStorage.setItem('user', "david");
+            localStorage.setItem('password', "david");
+            const usr = localStorage.getItem('user');
+            const psswd = localStorage.getItem('password');
+
+        const StatisticsView = () =>(
+            <Statistics/>
+        );
+        const RegisterView = () => (
+            <Register/>
+        );
         function verify() {
             if(document.getElementById('email').value == localStorage.getItem('user')  && document.getElementById('password').value == localStorage.getItem('password')){
                 localStorage.setItem('isLoggedIn', true);
@@ -23,55 +36,65 @@ export class Login extends React.Component{
             }else{
                 localStorage.setItem('isLoggedIn', false);
                 alert("Incorrect Credentials");
-
             }
         }
         return (
-            <React.Fragment>
-                <CssBaseline />
-                <main className="layout">
-                    <Paper className="paper">
-                        <Avatar className="avatar">
-                            <LockIcon />
-                        </Avatar>
-                        <Typography variant="headline">Hi! Sign in</Typography>
-                        <form className="form">
-                            <FormControl margin="normal" required fullWidth>
-                                <InputLabel htmlFor="email">Email Address</InputLabel>
-                                <Input id="email" name="email" autoComplete="email" autoFocus />
-                            </FormControl>
-                            <FormControl margin="normal" required fullWidth>
-                                <InputLabel htmlFor="password">Password</InputLabel>
-                                <Input
-                                    name="password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                />
-                            </FormControl>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="raised"
-                                color="primary"
-                                className="submit"
-                                onClick = {verify} >
-                                Sign in
-                            </Button>
-                            <br></br>
-                            <Link to="/Register"><Button
-                                type="reg"
-                                fullWidth
-                                variant="raised"
-                                color="primary"
-                                className="reg"
-                                >
-                                Register
-                            </Button></Link>
-                        </form>
-                    </Paper>
-                </main>
-            </React.Fragment>
+            <Router>
+                <React.Fragment>
+                    <CssBaseline />
+                    <main className="layout">
+                        <Paper className="paper">
+                            <Avatar className="avatar">
+                                <LockIcon />
+                            </Avatar>
+                            <Typography variant="headline">Hi! Sign in</Typography>
+                            <form className="form">
+                                <FormControl margin="normal" required fullWidth>
+                                    <InputLabel htmlFor="email">Email Address</InputLabel>
+                                    <Input id="email" name="email" autoComplete="email" autoFocus />
+                                </FormControl>
+                                <FormControl margin="normal" required fullWidth>
+                                    <InputLabel htmlFor="password">Password</InputLabel>
+                                    <Input
+                                        name="password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                    />
+                                </FormControl>
+                                <Button
+                                    component={Link}
+                                    to= "/Statistics"
+                                    type="submit"
+                                    fullWidth
+                                    variant="raised"
+                                    color="primary"
+                                    className="submit"
+                                    onClick = {verify} >
+                                    Sign in
+                                </Button>
+                                <br></br>
+                                <Button
+                                    component = {Link}
+                                    to="/Register"
+                                    type="submit"
+                                    fullWidth
+                                    variant="raised"
+                                    color="primary"
+                                    className="submit"
+                                    >
+                                    Register
+                                </Button>
+                                <h1>{localStorage.getItem('isLoggedIn')}</h1>
+                            </form>
+                            <div>
+                                <Route  path="/Register" component={RegisterView}/>
+                                {localStorage.getItem('isLoggedIn') && <Route path="/Statistics" component={StatisticsView}/>}
+                             </div>
+                        </Paper>
+                    </main>
+                </React.Fragment>
+            </Router>
         );
     }
 
