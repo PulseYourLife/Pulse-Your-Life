07bfba6f3037b01baf1e797d9748ec2f1d769264
danielDaleman <danieldaleman@hotmail.com>
2018-10-01 22:09:05 -0500

    View full

--- src/component/Statistics.js
+++ src/component/Statistics.js
@@ -10,69 +10,66 @@ import Healing from '@material-ui/icons/Healing';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import './Login.css'
+import Chart from './Chart';
 
 export class Statistics extends React.Component {
-  render() {
 
-    var data = {
-      labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
-      series: [
-        [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
-      ]
-    };
-
-    var options = {
-      high: 10,
-      low: -10,
-      axisX: {
-        labelInterpolationFnc: function(value, index) {
-          return index % 2 === 0 ? value : null;
+    constructor(){
+        super();
+        this.state={
+            charData:{}
         }
-      }
-    };
+    }
 
-    var type = 'Bar'		
+    componentWillMount(){
+        this.getChartData();
+    }
 
-	var simpleLineChartData = {
-	  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-	  series: [
-		[12, 9, 7, 8, 5],
-		[2, 1, 3.5, 7, 3],
-		[1, 3, 4, 5, 6]
-	  ]
-    }	
+    getChartData(){
+        this.setState({
+            //Ajax calls here
+            charData:{
+                labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5',
+                    'Week 6'],
+                datasets: [
+                    {
+                        label: 'Your Pulse',
+                        data: [
+                            212,
+                            123,
+                            345,
+                            245,
+                            234,
+                            231,
+                            789
+                        ],
+                        backgroundColor: [
+                            'rgba(128, 255, 96 , 0.6)',
+                            'rgba(54, 162, 235 , 0.6)',
+                            'rgba(255, 206, 86 , 0.6)',
+                            'rgba(75, 192, 192 , 0.6)',
+                            'rgba(153, 102, 255 , 0.6)',
+                            'rgba(255, 159, 64 , 0.6)',
+                            'rgba(255, 99, 132 , 0.6)'
+                        ]
+                    }
+                ]
+            }
+        });
+    }
 
-	var lineChartData = {
-		labels: [1, 2, 3, 4, 5, 6, 7, 8],
-		series: [
-			[5, 9, 7, 8, 5, 3, 5, 4]
-		]
-	}
-	
-	var lineChartOptions = {
-		low: 0,
-		showArea: true
-	}
+    render() {
+
+
 
     return (
+	
 	<React.Fragment>
                 <CssBaseline />
                 <main className="layout">
-                    <Paper className="paper">
-                        <Avatar className="avatar">
-                            <Healing />
-                        </Avatar>
-                        <Typography variant="headline"> MY HEART</Typography>                        																							
-                    </Paper>
-					<div>
-						<h1> Your Pulse </h1>
-						<ChartistGraph data={data} options={options} type={type} />
-						<h1> Your Pulse two</h1>
-						<ChartistGraph data={simpleLineChartData} type={'Line'} />
-						<h1> Your Pulse three</h1>
-						<ChartistGraph data={lineChartData} options={lineChartOptions} type={'Line'} />
-					  </div>
-					
+                    <div className="App">
+                        <Chart chartData={this.state.charData} legendPosition="bottom" />
+                    </div>
                 </main>
             </React.Fragment>
 	
